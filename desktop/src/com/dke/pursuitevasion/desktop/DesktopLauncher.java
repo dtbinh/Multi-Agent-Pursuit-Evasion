9fbe56bab7a6d9bdceb29ff0909f98e46a0c92d8
nicolagheza <ghzncl@gmail.com>
2017-02-08 12:50:01 +0100

    First commit

---
+++ desktop/src/com/dke/pursuitevasion/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dke.pursuitevasion.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dke.pursuitevasion.PursuitEvasion;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PursuitEvasion(), config);
+	}
+}
