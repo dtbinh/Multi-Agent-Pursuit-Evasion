9efd44be5e063e89cef71b5fa6acb8423fa5e324
Eric <ericchangeu@gmail.com>
2017-02-16 19:27:42 +0100

    Pressing enter constructs a mesh. Vertices can no longer be placed close to each other

--- core/src/com/dke/pursuitevasion/Editor/MapBuilderScreen.java
+++ core/src/com/dke/pursuitevasion/Editor/MapBuilderScreen.java
@@ -1,25 +1,26 @@
 package com.dke.pursuitevasion.Editor;
 
 import com.badlogic.gdx.*;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.PerspectiveCamera;
-import com.badlogic.gdx.graphics.VertexAttributes;
 import com.badlogic.gdx.graphics.g3d.*;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.math.EarClippingTriangulator;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Plane;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.math.collision.Ray;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.ShortArray;
 import com.dke.pursuitevasion.PursuitEvasion;
 import com.dke.pursuitevasion.TrackingCameraController;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Vector;
 
 /**
  * Created by jeeza on 10-2-17.
@@ -33,8 +34,14 @@ public class MapBuilderScreen implements Screen, InputProcessor {
     ModelBatch modelBatch;
     ModelBuilder modelBuilder;
     ArrayList<ModelInstance> instances;
+    ArrayList<Vector3> instanceVectors;
     TrackingCameraController trackingCameraController;
     Boolean leftPressed;
+    Mesh mesh;
+    Model model;
+    ModelInstance meshMap;
+    float[] vertList = new float[0];
+    int listSize;
     ShaderProgram shader;
     String vertexShader = "attribute vec4 a_position;    \n" +
             "attribute vec4 a_color;\n" +
@@ -85,7 +92,74 @@ public class MapBuilderScreen implements Screen, InputProcessor {
         modelBatch = new ModelBatch();
         modelBuilder = new ModelBuilder();
 
+        mesh = new Mesh(true, 4, 6, new VertexAttribute(VertexAttributes.Usage.Position, 3, "a_position"));
+        //mesh.setVertices(new float[]{-1f, -1f, 0f,0f, -1f, 0f,0f, 0f, 0f,-1f, 0f, 0f,});
+        mesh.setVertices(new float[]{0f, 0f, 0f,0f, 0f, 0,0f, 0f, 0f,0f, 0f, 0f,});
+        mesh.setIndices(new short[] {0, 1, 2, 2, 3, 0,});
+        Material material = new Material(ColorAttribute.createDiffuse(Color.WHITE));
+        modelBuilder.begin();
+        modelBuilder.part("Temp", mesh, GL20.GL_TRIANGLES, material);
+        model = modelBuilder.end();
+        meshMap = new ModelInstance(model, 0,0,0);
+
         instances = new ArrayList<ModelInstance>();
+        instanceVectors = new ArrayList<Vector3>();
+    }
+    public boolean nearNeighbor(Vector3 vec){
+        for(int i=0;i<instanceVectors.size();i++){
+            Vector3 v = instanceVectors.get(i);
+            if (vec.x<v.x+0.5 && vec.x>v.x-0.5 && vec.z<v.z+0.5 && vec.z>v.z-0.5){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void remakeMesh(){
+        //Remove y float from vertList
+        int newLength = (2*vertList.length)/3;
+        int forLoopNum = (vertList.length/3);
+        float[] newVertList  = new float[newLength];
+        ArrayList<Float> temp = new ArrayList<Float>();
+        //System.out.println(vertList.length);
+        for (int i=0; i<forLoopNum; i++) {
+            temp.add(vertList[i*3]);
+            temp.add(vertList[i*3+2]);
+        }
+        for (int i=0; i<temp.size(); i++)
+        {
+            newVertList[i] = temp.get(i);
+        }
+
+        EarClippingTriangulator triangulator = new EarClippingTriangulator();
+        ShortArray meshIndices = triangulator.computeTriangles(newVertList);
+
+        short[] indices = new short[meshIndices.size];
+        for (int i=0; i<meshIndices.size;i++)
+        {
+            indices[i] = meshIndices.get(i);
+            System.out.print(indices[i]+ " ");
+            System.out.println(meshIndices.get(i));
+        }
+
+        mesh = new Mesh(true, vertList.length, meshIndices.size,
+                new VertexAttribute(VertexAttributes.Usage.Position, 3, "a_position"));
+        mesh.setVertices(vertList);
+        mesh.setIndices(indices);
+        Material material = new Material(ColorAttribute.createDiffuse(Color.GOLD));
+        modelBuilder.begin();
+        modelBuilder.part("Course", mesh, GL20.GL_TRIANGLES, material);
+        model = modelBuilder.end();
+        meshMap = new ModelInstance(model, 0,0,0);
+    }
+
+    public void resizeArray(float[] oldVertList) {
+        // create a new array of size+3
+        int newSize = oldVertList.length + 3;
+        float[] newArray = new float[newSize];
+        System.arraycopy(oldVertList, 0, newArray, 0, oldVertList.length);
+        vertList = newArray;
     }
 
     @Override
@@ -109,6 +183,7 @@ public class MapBuilderScreen implements Screen, InputProcessor {
         shader.end();*/
 
         modelBatch.begin(camera);
+        modelBatch.render(meshMap, environ);
         for (int i=0;i<instances.size();i++)
         {
             modelBatch.render(instances.get(i), environ);
@@ -152,6 +227,10 @@ public class MapBuilderScreen implements Screen, InputProcessor {
 
     @Override
     public boolean keyUp(int keycode) {
+        if (keycode == Input.Keys.ENTER) {
+            remakeMesh();
+            return true;
+        }
         return false;
     }
 
@@ -175,13 +254,26 @@ public class MapBuilderScreen implements Screen, InputProcessor {
             Ray pickRay = camera.getPickRay(screenX, screenY);
             Vector3 intersection = new Vector3();
             Intersector.intersectRayPlane(pickRay, new Plane(new Vector3(0f, 1f, 0f), 0f), intersection);
-            System.out.println(intersection);
+
+            String verts = intersection.toString();
+            verts = verts.replaceAll("[()]","");
+            verts = verts.replaceAll("[,]",", ");
+            String[] Array = verts.split(",");
+            resizeArray(vertList);
+            for(int i = 0; i < 3; i++) {
+                vertList[listSize+i] = Float.parseFloat(Array[i]);
+            }
+            listSize+=3;
 
             //Create a model at position
             Model vertexPos = modelBuilder.createSphere(0.15f, 0.15f, 0.15f, 20, 20, new Material(ColorAttribute.createDiffuse(Color.LIGHT_GRAY)),
                     VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
-            ModelInstance vPosInst = new ModelInstance(vertexPos, intersection);
-            instances.add(vPosInst);
+
+            if(!nearNeighbor(intersection)) {
+                ModelInstance vPosInst = new ModelInstance(vertexPos, intersection);
+                instances.add(vPosInst);
+                instanceVectors.add(intersection);
+            }
         }
 
         leftPressed = false;
