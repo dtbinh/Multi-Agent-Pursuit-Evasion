a2406c750d3e7cb69e5f926beabdde26064cdf14
Eric <ericchangeu@gmail.com>
2017-03-23 12:57:10 +0100

    Added wallinfo class to store, you guessed it, wall info

---
+++ core/src/com/dke/pursuitevasion/WallInfo.java
@@ -0,0 +1,15 @@
+package com.dke.pursuitevasion;
+
+import com.badlogic.gdx.math.Vector3;
+
+/**
+ * Created by Envy on 3/23/2017.
+ */
+public class WallInfo {
+    public float length;
+    public float rotAngle;
+    public float height;
+    public Vector3 position;
+    public Vector3 start;
+    public Vector3 end;
+}
