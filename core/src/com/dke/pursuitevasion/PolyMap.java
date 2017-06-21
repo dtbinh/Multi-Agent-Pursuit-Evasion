9a5daa85be8f55d764635be6ad86e18a98b67226
callumclark <callum.clark@live.co.uk>
2017-06-21 13:08:59 +0200

    Map gneration

--- core/src/com/dke/pursuitevasion/PolyMap.java
+++ core/src/com/dke/pursuitevasion/PolyMap.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Mesh;
 import com.badlogic.gdx.graphics.VertexAttribute;
 import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.math.DelaunayTriangulator;
+import com.badlogic.gdx.utils.ShortArray;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -62,6 +64,9 @@ public class PolyMap {
         eV = edgeVectorsInfo.toArray(new EdgeVectors[edgeVectorsInfo.size()]);
     }
 
+
+
+
     public WallInfo[] getwI() {
         return wI;
     }
@@ -82,4 +87,117 @@ public class PolyMap {
         eI = evaderInfo.toArray(new EvaderInfo[evaderInfo.size()]);
     }
 
+    public Mesh generateMesh(){
+        polyVertexArray = generatePolyVertexArray();
+        //polyIndexArray = generatePolyIndexArray();
+        polyIndexArray = squarePolyIndexArray();
+        VertexAttributes attributes = new VertexAttributes(VertexAttribute.Position(), VertexAttribute.TexCoords(0));
+
+        Mesh mesh = new Mesh(true, polyVertexArray.length, polyIndexArray.length, attributes);
+
+        mesh.setVertices(polyVertexArray);
+        mesh.setIndices(polyIndexArray);
+
+        System.out.println(polyIndexArray);
+        System.out.println(polyVertexArray);
+        System.out.println(mesh);
+
+        return mesh;
+    }
+
+    private float[] generatePolyVertexArray(){
+        float[] polyVertexArray = new float[20];
+
+        for(int i=1; i<polyVertexArray.length;i+=5){
+            System.out.println(polyVertexArray.length+"  "+i);
+            if(i==1){
+                polyVertexArray[i-1] = (float) Math.random()*-10;
+                polyVertexArray[i]=0;
+                polyVertexArray[i+1] = (float) Math.random()*-10;
+                polyVertexArray[i+2] = 0;
+                polyVertexArray[i+3] = 0;
+            }else if(i==6){
+                polyVertexArray[i-1] = (float) Math.random()*-10;
+                polyVertexArray[i]=0;
+                polyVertexArray[i+1] = (float) Math.random()*10;
+                polyVertexArray[i+2] = 0;
+                polyVertexArray[i+3] = 0;
+            }else if(i==11) {
+                polyVertexArray[i-1] = (float) Math.random()*10;
+                polyVertexArray[i]=0;
+                polyVertexArray[i+1] = (float) Math.random()*10;
+                polyVertexArray[i+2] = 0;
+                polyVertexArray[i+3] = 0;
+            }else if(i==16){
+                polyVertexArray[i-1] = (float) Math.random()*10;
+                polyVertexArray[i]=0;
+                polyVertexArray[i+1] = (float) Math.random()*-10;
+                polyVertexArray[i+2] = 0;
+                polyVertexArray[i+3] = 0;
+
+            }
+        }
+
+        for(int j=0; j<polyVertexArray.length;j++){
+            System.out.println(polyVertexArray[j]);
+        }
+
+        return polyVertexArray;
+    }
+
+ /*   private float[] generatePolyVertexArray(){
+        float[] polyVertexArray = new float[20];
+
+        for(int i=0; i<polyVertexArray.length; i++){
+            float rand = (float) (Math.random()*3);
+            if(i==1 || i==6 || i==11 || i==16){
+                polyVertexArray[i] = 0;
+            }else if(i==0||i==2||i==3||i==5||i==8||i==9||i==14||i==17){
+                if(i==2||i==17){
+                    rand = (float) Math.random()+2;
+                }
+                polyVertexArray[i] = -1*rand;
+            }else if(i==10 || i==15){
+                polyVertexArray[i] = (float) Math.random()+4;
+            }else if(i==7 || i==12){
+                polyVertexArray[i] = (float) Math.random()+2;
+            }
+            else{
+                polyVertexArray[i] = rand;
+            }
+
+        }
+        return polyVertexArray;
+    }*/
+
+    private short[] generatePolyIndexArray(){
+        short[] polyIndexArray = new short[6];
+        for(int i=0; i<polyIndexArray.length; i++){
+            int rand = (int) Math.floor((Math.random()*4));
+            polyIndexArray[i] = (short) rand;
+        }
+        return polyIndexArray;
+    }
+
+    private short[] squarePolyIndexArray(){
+        short[] polyIndexarray = new short[6];
+        polyIndexarray[0] = 3;
+        polyIndexarray[1] = 0;
+        polyIndexarray[2] = 2;
+        polyIndexarray[3] = 0;
+        polyIndexarray[4] = 1;
+        polyIndexarray[5] = 2;
+        return polyIndexarray;
+    }
+
+    /*
+
+    private int[] indexArrayToInt(short[] polyIndexArray){
+        int [] intArray = new int[polyIndexArray.length];
+        for (int i=0; i<polyIndexArray.length;i++){
+            intArray[i] =(int) polyIndexArray[i];
+        }
+        return intArray;
+    }
+  */
 }
